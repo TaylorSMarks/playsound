5b04233ed264618b5e748394c223b6777beab337
Alexandr <alexandrk95@gmail.com>
2016-11-25 16:56:02 +0300

    Fix encoding for Windows

--- playsound.py
+++ playsound.py
@@ -16,17 +16,18 @@ def _playsoundWin(sound, block = True):
     from ctypes import c_buffer, windll
     from random import random
     from time   import sleep
+    from sys    import getfilesystemencoding
 
     def winCommand(*command):
         buf = c_buffer(255)
-        command = ' '.join(command).encode()
+        command = ' '.join(command).encode(getfilesystemencoding())
         errorCode = int(windll.winmm.mciSendStringA(command, buf, 254, 0))
         if errorCode:
             errorBuffer = c_buffer(255)
             windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
             exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
-                                '\n        ' + command +
-                                '\n    ' + errorBuffer.value)
+                                '\n        ' + command.decode() +
+                                '\n    ' + errorBuffer.value.decode())
             raise PlaysoundException(exceptionMessage)
         return buf.value
 
@@ -34,7 +35,7 @@ def _playsoundWin(sound, block = True):
     winCommand('open "' + sound + '" alias', alias)
     winCommand('set', alias, 'time format milliseconds')
     durationInMS = winCommand('status', alias, 'length')
-    winCommand('play', alias, 'from 0 to', durationInMS)
+    winCommand('play', alias, 'from 0 to', durationInMS.decode())
 
     if block:
         sleep(float(durationInMS) / 1000.0)
@@ -79,7 +80,7 @@ def _playsoundNix(sound, block = True):
     import ossaudiodev
     from sys  import byteorder
     from wave import open as waveOpen, AFMT_S16_LE, AFMT_S16_BE
-    
+
     with waveOpen(sound, 'rb') as sound:
         channelCount, sampleWidth, framerate, frameCount, compressionType, compressionName = sound.getparams()
         try:
