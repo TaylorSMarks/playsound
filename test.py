2484c656b44965d5b67c77fb70c3aef5a0512840
Elaina <userelaina@pm.me>
2023-03-28 13:52:24 +0000

    add tests without MCI

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ if isTravis and system == 'Windows':
         # versions require python 3.3, utterly defeating the purpose of making
         # the library available on pypi.
         pipmain(['install', 'mock==2.0.0'])
-        from mock   import patch
+        from mock import patch
 
 from playsound import playsound, PlaysoundException
 import unittest
@@ -63,35 +63,52 @@ class PlaysoundTests(unittest.TestCase):
         print(path.encode('utf-8'))
         return path
 
-    def helper(self, file, approximateDuration, block = True):
+    def helper(self, file, approximateDuration, block = True, usingMciSendStringW = True):
         startTime = time()
         path = self.get_full_path(file)
 
         if isTravis and system == 'Windows':
-            with patch('ctypes.windll.winmm.mciSendStringW', side_effect = mockMciSendStringW):
-                global expectedDuration, sawClose, testCase
-                testCase = self
-                sawClose = False
-                expectedDuration = approximateDuration
+            if usingMciSendStringW:
+                with patch('ctypes.windll.winmm.mciSendStringW', side_effect = mockMciSendStringW):
+                    global expectedDuration, sawClose, testCase
+                    testCase = self
+                    sawClose = False
+                    expectedDuration = approximateDuration
+                    playsound(path, block = block)
+                    self.assertTrue(sawClose)
+            else:
                 playsound(path, block = block)
-                self.assertTrue(sawClose)
+                sleep(3)
         else:
             playsound(path, block = block)
         duration = time() - startTime
         self.assertTrue(approximateDuration - durationMarginLow <= duration <= approximateDuration + duratingMarginHigh, 'File "{}" took an unexpected amount of time: {:.2f} - expected ~{:.2f}'.format(file.encode('utf-8'), duration, approximateDuration))
 
     testBlockingASCII_MP3 = lambda self: self.helper('Damonte.mp3', 1.1)
-    testBlockingASCII_WAV = lambda self: self.helper('Sound4.wav',  1.3)
+    testBlockingASCII_WAV = lambda self: self.helper('Sound4.wav', 1.3)
     testBlockingCYRIL_WAV = lambda self: self.helper(u'Буква_Я.wav', 1.6)
     testBlockingSPACE_MP3 = lambda self: self.helper('Discovery - Go at throttle up (2).mp3', 2.3)
-    testNonBlockingRepeat = lambda self: self.helper(u'Буква_Я.wav', 0.0, block = False)
+    testNonBlockingASCII_MP31 = lambda self: self.helper('Damonte.mp3', 0.0, block = False)
+    testNonBlockingASCII_WAV1 = lambda self: self.helper('Sound4.wav', 0.0, block = False)
+    testNonBlockingCYRIL_WAV1 = lambda self: self.helper(u'Буква_Я.wav', 0.0, block = False)
+    testNonBlockingSPACE_MP31 = lambda self: self.helper('Discovery - Go at throttle up (2).mp3', 0.0, block = False)
+    if system == 'Windows':
+        testNonBlockingASCII_MP30 = lambda self: self.helper('Damonte.mp3', 0.0, block = False, usingMciSendStringW = False)
+        testNonBlockingASCII_WAV0 = lambda self: self.helper('Sound4.wav', 0.0, block = False, usingMciSendStringW = False)
+        testNonBlockingCYRIL_WAV0 = lambda self: self.helper(u'Буква_Я.wav', 0.0, block = False, usingMciSendStringW = False)
+        testNonBlockingSPACE_MP30 = lambda self: self.helper('Discovery - Go at throttle up (2).mp3', 0.0, block = False, usingMciSendStringW = False)
 
     def testMissing(self):
         with self.assertRaises(PlaysoundException) as context:
             playsound(self.get_full_path('fakefile.wav'))
-
         message = str(context.exception).lower()
-            
+        for sub in ['not', 'fakefile.wav']:
+            self.assertIn(sub, message, '"{}" was expected in the exception message, but instead got: "{}"'.format(sub, message))
+
+    def testMissing2(self):
+        with self.assertRaises(PlaysoundException) as context:
+            playsound(self.get_full_path('fakefile.wav'), block = False)
+        message = str(context.exception).lower()
         for sub in ['not', 'fakefile.wav']:
             self.assertIn(sub, message, '"{}" was expected in the exception message, but instead got: "{}"'.format(sub, message))
 
